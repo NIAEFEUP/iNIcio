fa28a0f6dae95bd94ccc00205abd05f7a276b35f
diogotvf7 <diogotvf7@gmail.com>
2025-07-23 14:19:10 +0100

    Responsive landing page; Responsive navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,4 +1,9 @@
+"use client"
+
 import Link from "next/link";
+import { useState } from "react";
+
+import { ChevronUp, ChevronDown } from "lucide-react"; 
 
 type Props = {
   className?: string;
@@ -6,15 +11,34 @@ type Props = {
 };
 
 export default function Navbar({ className, role }: Props) {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
   const isAuthenticated = true;
 
-  return (
-    <div className={`${className} flex justify-between p-5 text-2xl shadow-sm`}>
-      <Link href="/">
-        <img src="/logo.svg" />
-      </Link>
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
-      <div className="flex gap-5">
+  return (
+    <div className={`${className} flex flex-col md:flex-row justify-between p-5 text-2xl shadow-sm`}>
+      <div className="flex justify-between items-center">
+        <Link href="/">
+          <img src="/logo.svg" alt="Logo" />
+        </Link>
+        <button
+          className="md:hidden text-2xl"
+          onClick={toggleMenu}
+          aria-label="Toggle menu"
+        >
+          {isMenuOpen ? (<ChevronUp />) : (<ChevronDown />)}
+        </button>
+      </div>
+
+      {/* Menu options */}
+      <div
+        className={`flex-col md:flex-row flex gap-5 transition-all duration-300 ease-in-out ${
+          isMenuOpen ? "mt-5 opacity-100 max-h-screen" : "opacity-0 max-h-0 overflow-hidden"
+        } md:flex`}
+      >
         <Link href="https://niaefeup.pt">Site do NI</Link>
         <Link href="/alocacoes">Alocações</Link>
         <Link href="/candidatos">Candidatos</Link>
@@ -35,9 +59,7 @@ export default function Navbar({ className, role }: Props) {
           <Link className="text-primary" href="/admin">
             <span className="text-primary">AdminUI</span>
           </Link>
-        ) : (
-          <></>
-        )}
+        ) : null}
       </div>
     </div>
   );
