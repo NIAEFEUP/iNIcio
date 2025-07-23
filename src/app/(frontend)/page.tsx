76fc8508bdff679e3ad47f39082a4f054ae9b432
diogotvf7 <diogotvf7@gmail.com>
2025-07-23 12:06:39 +0100

    Landing page started

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,59 +1,43 @@
-import { headers as getHeaders } from 'next/headers.js'
-import Image from 'next/image'
-import { getPayload } from 'payload'
-import React from 'react'
-import { fileURLToPath } from 'url'
+import { headers as getHeaders } from "next/headers.js";
+import Image from "next/image";
+import { getPayload } from "payload";
+import React from "react";
+import { fileURLToPath } from "url";
 
-import config from '@/payload.config'
-import './styles.css'
+import config from "@/payload.config";
+import "./styles.css";
+import { Button } from "@/components/ui/button";
 
 export default async function HomePage() {
-  const headers = await getHeaders()
-  const payloadConfig = await config
-  const payload = await getPayload({ config: payloadConfig })
-  const { user } = await payload.auth({ headers })
+  const headers = await getHeaders();
+  const payloadConfig = await config;
+  const payload = await getPayload({ config: payloadConfig });
+  const { user } = await payload.auth({ headers });
 
-  const fileURL = `vscode://file/${fileURLToPath(import.meta.url)}`
+  const fileURL = `vscode://file/${fileURLToPath(import.meta.url)}`;
 
   return (
-    <div className="home">
-      <div className="content">
-        <picture>
-          <source srcSet="https://raw.githubusercontent.com/payloadcms/payload/main/packages/ui/src/assets/payload-favicon.svg" />
-          <Image
-            alt="Payload Logo"
-            height={65}
-            src="https://raw.githubusercontent.com/payloadcms/payload/main/packages/ui/src/assets/payload-favicon.svg"
-            width={65}
-          />
-        </picture>
-        {!user && <h1>Welcome to your new project.</h1>}
-        {user && <h1>Welcome back, {user.email}</h1>}
-        <div className="links">
+    <div className="h-full flex flex-col items-center justify-center">
+      <h1 className="font-bold text-9xl w-1/2 text-center">
+        Queres fazer parte do <span className="text-primary">NIAEFEUP</span>?
+      </h1>
+      <div className="flex justify-center">
+        <button className="mt-10 rounded-full text-primary">
+          <a href="registo">Registar-te →</a>
+        </button>
+        <Button className="mt-10">
+          <a href="registo">Regista-te !</a>
+        </Button>
+        <Button className="mt-10" asChild>
           <a
-            className="admin"
-            href={payloadConfig.routes.admin}
-            rel="noopener noreferrer"
+            href="https://niaefeup.pt/"
             target="_blank"
-          >
-            Go to admin panel
-          </a>
-          <a
-            className="docs"
-            href="https://payloadcms.com/docs"
             rel="noopener noreferrer"
-            target="_blank"
           >
-            Documentation
+            Visita o nosso site !
           </a>
-        </div>
-      </div>
-      <div className="footer">
-        <p>Update this page by editing</p>
-        <a className="codeLink" href={fileURL}>
-          <code>app/(frontend)/page.tsx</code>
-        </a>
+        </Button>
       </div>
     </div>
-  )
+  );
 }
