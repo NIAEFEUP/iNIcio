4aaa73c18798b3289aacfd3c60df58ae51c96cf9
Tomás Palma <up202108880@edu.fe.up.pt>
2025-07-23 15:24:13 +0100

    chore: add betterauth drizzle schema

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,7 +3,8 @@ import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
   out: './src/drizzle',
-  schema: './src/db/schema.ts',
+  schema: './src/db/schema',
+  casing: 'snake_case',
   dialect: 'postgresql',
   dbCredentials: {
     url: process.env.DATABASE_URL!,
