9720fa36e7246b5e1eaed4a5a9486919c4c86f30
Tomás Palma <up202108880@edu.fe.up.pt>
2025-07-27 12:35:30 +0100

    chore: remove unused signup route outside betterauth

--- src/app/api/auth/signup/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextApiRequest } from "next";
-import { NextResponse } from "next/server";
-import { authClient } from "@/lib/auth-client";
-
-export async function POST(req: NextApiRequest) {
-  console.log(req);
-
-  return NextResponse.json({ hello: "world" });
-}
