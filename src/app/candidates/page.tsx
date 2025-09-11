ececf539add6e033ef62f9eac21bf3616a6254e4
Tomás Palma <up202108880@edu.fe.up.pt>
2025-09-11 11:34:01 +0100

    feat: search in candidates page

---
+++ src/app/candidates/page.tsx
@@ -0,0 +1,17 @@
+import CandidatesClient from "@/components/friends/candidates-client";
+import { auth } from "@/lib/auth";
+import { getAllCandidateUsers } from "@/lib/db";
+import { getFriendsOf } from "@/lib/friend";
+import { headers } from "next/headers";
+
+export default async function Friends() {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  const candidates = await getAllCandidateUsers();
+
+  const friends = await getFriendsOf(session ? session.user.id : "");
+
+  return <CandidatesClient candidates={candidates} friends={friends} />;
+}
