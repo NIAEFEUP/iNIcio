b38ddcedab75b6e981d7adc249c69b34915f86cf
Tomás Palma <up202108880@edu.fe.up.pt>
2025-09-24 22:56:04 +0100

    fix: navbar crashing

--- src/components/navbar-controller.tsx
+++ src/components/navbar-controller.tsx
@@ -1,7 +1,16 @@
 "use server";
 
+import { headers } from "next/headers";
 import Navbar from "./navbar";
+import { isAdmin } from "@/lib/admin";
+import { auth } from "@/lib/auth";
 
 export default async function NavbarController() {
-  return <Navbar />;
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  const admin = await isAdmin(session?.user.id);
+
+  return <Navbar isAdmin={admin ? true : false} />;
 }
