19055a8b2f059416205b8424f2631b8f44dafe14
Tomás Palma <up202108880@edu.fe.up.pt>
2025-09-26 16:53:13 +0100

    fix: format

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,9 +16,8 @@ const eslintConfig = [
     rules: {
       "@next/next/no-img-element": "off",
       "@typescript-eslint/no-explicit-any": "off",
-    }
-  }
+    },
+  },
 ];
 
 export default eslintConfig;
-
