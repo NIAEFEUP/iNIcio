3d6d4f8b222d9d8e8fb1465a501b294ba7d9aa38
Tomás Palma <up202108880@edu.fe.up.pt>
2025-09-29 22:46:39 +0100

    fix: wrong logic in showing either candidate or recruiter homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,7 +24,7 @@ export default async function Home() {
       {recruitmentActive ? (
         <>
           <RecruitmentActiveMessage
-            isRecruiter={recruiter !== undefined || recruiter !== null}
+            isRecruiter={recruiter !== undefined && recruiter !== null}
           />
         </>
       ) : (
