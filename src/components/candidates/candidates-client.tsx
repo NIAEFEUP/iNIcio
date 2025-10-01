47c6186871749c389aef90b196a6dc272ce9f6a1
Tomás Palma <up202108880@edu.fe.up.pt>
2025-10-01 19:05:44 +0100

    feat: better grid for candidates page

--- src/components/candidates/candidates-client.tsx
+++ src/components/candidates/candidates-client.tsx
@@ -270,7 +270,7 @@ export default function CandidatesClient({
         )}
       </div>
 
-      <div className="mx-auto w-full max-w-[80em] grid grid-cols-1 sm:grid-cols-2 md:grid-cols-5 lg:grid-cols-5 xl:grid-cols-5 gap-4">
+      <div className="mx-auto w-full max-w-[80em] grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-5 gap-4">
         {filteredCandidates.map(
           (
             candidate: User & {
