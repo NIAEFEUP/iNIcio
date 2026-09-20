a78dc11bdf0e1b364feafdafe5c47d28607ad240
Pedro Castro <falecompedroac@gmail.com>
2026-09-20 22:44:21 +0100

    fix: deduplicate availability migration

--- drizzle/0005_rapid_storm.sql
+++ drizzle/0005_rapid_storm.sql
@@ -1,1 +1,9 @@
+DELETE FROM "recruiter_availability" a
+USING "recruiter_availability" b
+WHERE a.id > b.id
+  AND a."start" = b."start"
+  AND a.duration = b.duration
+  AND a.recruiter_id = b.recruiter_id
+  AND a.recruitment_id = b.recruitment_id;
+
 ALTER TABLE "recruiter_availability" ADD CONSTRAINT "recruiter_availability_unique" UNIQUE("start","duration","recruiter_id","recruitment_id");
